0ae2e3d73b12e9947804165d2a4669c6966ab4c9
Swezzy491 <semihbilgin0534@gmail.com>
2024-12-05 15:21:55 +0300

    Version_4
    BİG UPDATE 
    MÜDÜR 
    ÖGRENCİ 
    ÖGRETMEN FORMU

---
+++ Kitaplik/veritaban--main/veritaban--main/Kitaplik/Kitaplik/ogrenci_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitaplik
+{
+    public partial class ogrenci_form : Form
+    {
+        public ogrenci_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
